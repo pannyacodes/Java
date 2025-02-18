8370bf41e09e31d0a3baa0727aefd76ec2f9e7a7
= <=>
2025-02-18 17:26:45 +0530

    java

---
+++ day 02/Datatypes.java
@@ -0,0 +1,9 @@
+class Datatypes{
+    public static void main(String args[]){
+        // double weight = 60.5;
+        // System.out.println(weight);
+        // int studentnumber = 25;
+        // boolean switchOn = true;
+        String studentname = "name";
+    }
+}
---
+++ day 02/Sum01.java
@@ -0,0 +1,8 @@
+class Sum01{
+    public static void main(String args[]){
+        int x=2;
+        int y=3;
+        int sum=x+y;
+        System.out.println(sum);
+    }
+}
